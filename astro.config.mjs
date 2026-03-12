097f67b1730d1a8b60c397377c092e5773a4c1b7
RoterBaronH <RoterBaronH@users.noreply.github.com>
2026-03-12 23:09:07 +0100

    switch to custom domain www.hideakimtg.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://RoterBaronH.github.io',
-  base: '/Moltencollection/',
+  site: 'https://www.hideakimtg.com',
 });
